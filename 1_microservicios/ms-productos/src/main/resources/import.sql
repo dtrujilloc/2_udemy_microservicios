2668755707ac0ecfd06c723d2b832ba4c334ad0a
dtrujillo <danieltrujillodev@gmail.com>
2022-07-26 18:26:44 -0500

    se crea archivo de carga inicial para la BD H2

---
+++ 1_microservicios/ms-productos/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES ('panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES ('Sony', 700, NOW());
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES ('Apple', 1000, NOW());
